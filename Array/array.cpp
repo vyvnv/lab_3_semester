cc5f019e29ea2799b4302deca8e36e17b1d29d40
vyvnv <galdanova.ayana.a@mail.ru>
2023-05-17 09:02:31 +0700

    first commit

---
+++ Array/array.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Array
+{ int *a, n;
+    n = 5;
+    public:
+    // конструктор 1
+    // len – число элементов в массиве
+    // t = 1 – неупорядоченный массив
+    // t = 2 – массив, упорядоченный по неубыванию
+    // t = 3 – массив, упорядоченный по невозрастанию
+    // d – диапазон псевдослучайных чисел для неупорядоченного массива (при t = 1)
+    Array(int len = 1, int t = 1, int d = 10);
+
+    Array(int *, int); // конструктор 2: по массиву
+    Array(Array &);
+    ~Array();
+    Array& operator = (Array &);
+    int &operator [](int);
+
+    bool Test(); // проверка на упорядоченность по неубыванию
+    bool operator == (Array); // равенство элементов массивов (но не порядка)
+
+    void Shell_sort();
+    void Heapsort();
+    void Hoar_sort();
+    void Bit_sort();
+    friend istream & operator >> (istream &, Array &);
+    friend ostream & operator << (ostream &, Array &);
+};
+
+Array::Array(int len = 1, int t = 1, int d = 10) 
+    {
+
+    }
